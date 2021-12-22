503b1fe3e50f51b5af1b0f45b16c3f83470fde45
Olivier Hoareau <oha@greenberets.io>
2021-12-22 15:59:04 +0100

    feat: dashboards

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource aws_cloudwatch_dashboard "dashboard" {
-  for_each       = toset(var.dashboards)
+  for_each       = {for k, v in var.dashboards: k => v}
   dashboard_name = each.key
   dashboard_body = lookup(each.value, "definition", "{}")
 }
